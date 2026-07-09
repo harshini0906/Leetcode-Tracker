6a24818013a9c9a057e4556c734bbbd2f17a488b
harshini0906 <gayaharitha84@gmail.com>
2026-07-09 15:10:23 +0530

    Create 184-DepartmentHighestSalary

---
+++ 184-DepartmentHighestSalary/184-DepartmentHighestSalary.sql
@@ -0,0 +1,8 @@
+-- Last updated: 09/07/2026, 15:11:17
+# Write your MySQL query statement below
+SELECT Department.name AS Department ,Employee.name AS Employee, Employee.salary
+FROM Department  
+JOIN Employee  
+ON Employee.departmentId=Department.id 
+WHERE(departmentId, salary) IN
+(SELECT departmentId,MAX(salary) FROM Employee GROUP BY departmentId) ;
