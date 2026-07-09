501cdb799f02185b09f3f5d0806958d0022cdfa2
harshini0906 <gayaharitha84@gmail.com>
2026-07-09 15:06:54 +0530

    Create 1415-StudentsAndExaminations

---
+++ 1415-StudentsAndExaminations/1415-StudentsAndExaminations.sql
@@ -0,0 +1,8 @@
+-- Last updated: 09/07/2026, 15:07:47
+# Write your MySQL query statement below
+select Students.student_id, Students.student_name, Subjects.subject_name, 
+count(Examinations.subject_name) as attended_exams
+from Students cross join Subjects
+left join Examinations on Students.student_id=Examinations.student_id and Subjects.subject_name=Examinations.subject_name
+group by Students.student_id, Students.student_name, Subjects.subject_name
+order by Students.student_id, Subjects.subject_name;
