17672b55e5f788c7e7f8f7ed88925a08cbbd6e6f
harshini0906 <gayaharitha84@gmail.com>
2026-07-09 15:10:52 +0530

    Create 155-MinStack

---
+++ 155-MinStack/155-MinStack.java
@@ -0,0 +1,37 @@
+// Last updated: 09/07/2026, 15:11:46
+class MinStack {
+    public Stack <Integer> stack;
+    public Stack <Integer> mini;
+    public MinStack() {
+        stack = new Stack();
+        mini = new Stack();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(mini.size()>0) mini.push(Math.min(mini.peek(),val));
+        else mini.push(val);
+    }
+    
+    public void pop() {
+        stack.pop();
+        mini.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return mini.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
