3ed9956ad45ea2acd7971a7cd65739256dfb4462
harshini0906 <gayaharitha84@gmail.com>
2026-07-09 15:08:38 +0530

    Create 455-AssignCookies

---
+++ 455-AssignCookies/455-AssignCookies.java
@@ -0,0 +1,16 @@
+// Last updated: 09/07/2026, 15:09:32
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int child=0;
+        int index=0;
+        while(index<s.length && child<g.length){
+            if(s[index]>=g[child]){
+                child++;
+            }
+            index++;
+        }
+        return child;
+    }
+}
