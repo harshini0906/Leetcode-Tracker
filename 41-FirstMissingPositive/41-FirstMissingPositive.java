e4d4394d3ed9dcff96893c9a839ba1d718007c67
harshini0906 <gayaharitha84@gmail.com>
2026-07-09 15:12:34 +0530

    Create 41-FirstMissingPositive

---
+++ 41-FirstMissingPositive/41-FirstMissingPositive.java
@@ -0,0 +1,18 @@
+// Last updated: 09/07/2026, 15:13:28
+class Solution {
+    void swap(int[] arr, int i, int j) {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public int firstMissingPositive(int[] nums) {
+        int n=nums.length;
+        for(int i=0; i<n; i++) {
+            while(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]) swap(nums,i,nums[i]-1);
+        }
+        for(int i=0; i<n; i++) {
+            if(nums[i]!=i+1) return i+1;
+        }
+        return n+1;
+    }
+}
