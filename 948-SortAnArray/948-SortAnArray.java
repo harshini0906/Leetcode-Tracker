0e42ceda02db5769f13856cc0bdfa7b328b62871
harshini0906 <gayaharitha84@gmail.com>
2026-07-09 15:07:39 +0530

    Create 948-SortAnArray

---
+++ 948-SortAnArray/948-SortAnArray.java
@@ -0,0 +1,15 @@
+// Last updated: 09/07/2026, 15:08:33
+class Solution {
+    public int[] sortArray(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = key;
+        }
+        return nums;
+    }
+}
