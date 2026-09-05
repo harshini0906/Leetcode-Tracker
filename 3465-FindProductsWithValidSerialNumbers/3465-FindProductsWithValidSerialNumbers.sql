1216e08d13e7744a41d20070d0a75074d3f0f788
harshini0906 <gayaharitha84@gmail.com>
2026-09-05 20:06:20 +0530

    Create 3465-FindProductsWithValidSerialNumbers

---
+++ 3465-FindProductsWithValidSerialNumbers/3465-FindProductsWithValidSerialNumbers.sql
@@ -0,0 +1,6 @@
+-- Last updated: 05/09/2026, 20:07:41
+SELECT product_id, product_name, description
+FROM products
+WHERE description REGEXP '\\bSN[0-9]{4}-[0-9]{4}\\b'
+      COLLATE utf8mb4_bin
+ORDER BY product_id;
