60effc7d1025d29d365789a40c9fb8248c9778d7
harshini0906 <gayaharitha84@gmail.com>
2026-09-05 20:08:20 +0530

    Create 2427-NumberOfCommonFactors

---
+++ 2427-NumberOfCommonFactors/2427-NumberOfCommonFactors.java
@@ -0,0 +1,11 @@
+// Last updated: 05/09/2026, 20:09:41
+class Solution {
+    public int commonFactors(int a, int b) {
+        int min=a<b?a:b;
+        int count=0;
+        for(int i=1; i<=min; i++) {
+            if(a%i==0 && b%i==0) count++;
+        }
+        return count;
+    }
+}
