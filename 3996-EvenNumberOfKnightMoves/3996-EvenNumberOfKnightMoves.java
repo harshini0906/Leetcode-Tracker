22c72cbce5780a8b64700237f48b6c2d545ecbc5
harshini0906 <gayaharitha84@gmail.com>
2026-09-05 20:04:18 +0530

    Create 3996-EvenNumberOfKnightMoves

---
+++ 3996-EvenNumberOfKnightMoves/3996-EvenNumberOfKnightMoves.java
@@ -0,0 +1,8 @@
+// Last updated: 05/09/2026, 20:05:39
+class Solution {
+    public boolean canReach(int[] start, int[] target) {
+        int s=(start[0]+start[1])%2;
+        int t=(target[0]+target[1])%2;
+        return s==t;
+    }
+}
