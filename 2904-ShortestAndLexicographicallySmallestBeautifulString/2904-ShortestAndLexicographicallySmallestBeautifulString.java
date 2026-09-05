248d32fa5e3291d950bf28f7ef6b0c835585db0b
harshini0906 <gayaharitha84@gmail.com>
2026-09-05 20:07:28 +0530

    Create 2904-ShortestAndLexicographicallySmallestBeautifulString

---
+++ 2904-ShortestAndLexicographicallySmallestBeautifulString/2904-ShortestAndLexicographicallySmallestBeautifulString.java
@@ -0,0 +1,27 @@
+// Last updated: 05/09/2026, 20:08:49
+class Solution {
+    public String shortestBeautifulSubstring(String s, int k) {
+        String ans = "";
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int oneCnt = 0;
+            StringBuilder cur = new StringBuilder();
+            for (int j = i; j < n; j++) {
+                cur.append(s.charAt(j));
+                if (s.charAt(j) == '1')
+                    oneCnt++;
+                if (oneCnt > k)
+                    break;
+                if (oneCnt == k) {
+                    String curStr = cur.toString();
+                    if (ans.isEmpty() ||
+                        curStr.length() < ans.length() ||
+                        (curStr.length() == ans.length() && curStr.compareTo(ans) < 0)) {
+                        ans = curStr;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
