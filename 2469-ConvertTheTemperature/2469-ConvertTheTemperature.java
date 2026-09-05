464336eda780862daddfb2caaded2ce452fc6d9f
harshini0906 <gayaharitha84@gmail.com>
2026-09-05 20:08:16 +0530

    Create 2469-ConvertTheTemperature

---
+++ 2469-ConvertTheTemperature/2469-ConvertTheTemperature.java
@@ -0,0 +1,6 @@
+// Last updated: 05/09/2026, 20:09:37
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        return new double[] {celsius+273.15, celsius*1.80+32.00};
+    }
+}
