0355b2ae0e113467edd81e47db727294ebd29b88
harshini0906 <gayaharitha84@gmail.com>
2026-09-05 20:07:38 +0530

    Create 2806-AccountBalanceAfterRoundedPurchase

---
+++ 2806-AccountBalanceAfterRoundedPurchase/2806-AccountBalanceAfterRoundedPurchase.cpp
@@ -0,0 +1,7 @@
+// Last updated: 05/09/2026, 20:08:58
+class Solution {
+public:
+    int accountBalanceAfterPurchase(int purchaseAmount) {
+        return 100-((purchaseAmount+5)/10)*10;
+    }
+};
