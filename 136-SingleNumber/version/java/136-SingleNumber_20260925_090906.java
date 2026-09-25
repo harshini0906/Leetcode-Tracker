121f4fe0f28e00ab52bc6810906dbf6d244afff3
harshini0906 <gayaharitha84@gmail.com>
2026-09-25 09:07:37 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/version/java/136-SingleNumber_20260925_090906.java
@@ -0,0 +1,25 @@
+// Last updated: 25/09/2026, 09:09:06
+1/**
+2 * Definition for singly-linked list.
+3 * class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public boolean hasCycle(ListNode head) {
+14        ListNode slow=head;
+15        ListNode fast=head;
+16        while(fast!=null && fast.next!=null) {
+17            slow=slow.next;
+18            fast=fast.next.next;
+19            if(slow==fast) return true;
+20        }
+21        return false;
+22    }
+23}
+24
